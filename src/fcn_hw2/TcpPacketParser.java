684e4990fb1cb2fb1ac06a8aedf35f9d9eb040bb
mukul2604 <mukul312@gmail.com>
2017-02-27 09:29:35 -0800

    ddsa

--- src/fcn_hw2/TcpPacketParser.java
+++ src/fcn_hw2/TcpPacketParser.java
@@ -1,6 +1,8 @@
 package fcn_hw2;
 
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
 
 /**
  * Created by mukul on 2/26/17.
@@ -9,24 +11,43 @@ import java.nio.ByteBuffer;
 public class TcpPacketParser {
     private int sourcePort;
     private int destinationPort;
-    private int seqNo;
-    private int ackNo;
+    private long seqNo;
+    private long ackNo;
     private int flags;
+    private int  temp;
 
+    private int byteArrayToUnsignedShort(byte[] b) {
+        return ((b[0] << 8 | b[1]) & 0xffff);
+
+    }
+
+    private long byteArrayToUnsignedInt(byte[] b) {
+        return ((b[0] << 24 | b[1] << 16 | b[2] << 8 | b[3]) & 0xffffffff);
+    }
+    
     public TcpPacketParser(byte[] tcpPacketArray){
-        byte [] arr = {tcpPacketArray[0], tcpPacketArray[1]};
-        ByteBuffer wrapped = ByteBuffer.wrap(arr);
-        this.sourcePort = wrapped.getShort();
-        byte [] arr1 = {tcpPacketArray[2], tcpPacketArray[3]};
-        ByteBuffer wrapped1 = ByteBuffer.wrap(arr1);
-        this.destinationPort = wrapped1.getShort();
-        this.seqNo = tcpPacketArray[0];
-        this.ackNo = tcpPacketArray[0];
+        byte[] subArr;
+        subArr = Arrays.copyOfRange(tcpPacketArray,0,2);
+        this.sourcePort = byteArrayToUnsignedShort(subArr);
+
+        subArr = Arrays.copyOfRange(tcpPacketArray,2,4);
+        this.destinationPort = byteArrayToUnsignedShort(subArr);
+
+        subArr = Arrays.copyOfRange(tcpPacketArray,4,8);
+        this.seqNo = byteArrayToUnsignedInt(subArr);
+
+        subArr = Arrays.copyOfRange(tcpPacketArray,8,12);
+        this.ackNo = byteArrayToUnsignedInt(subArr);
         this.flags = tcpPacketArray[0];
     }
 
+
     public void printPacket() {
+        System.out.println("////////////////////////////////");
         System.out.println("Source:" + sourcePort);
         System.out.println("Destination: " + destinationPort);
+        System.out.println("Seqno: " + seqNo);
+        System.out.println("Ack: " + ackNo);
+        System.out.println("////////////////////////////////");
     }
 }
